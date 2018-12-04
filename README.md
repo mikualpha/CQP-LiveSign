6b66e03fe43b7506ffd4e17ccc9c8b6d58262a43
MikuAlpha <mikualpha@hotmail.com>
2018-12-04 20:12:47 +0800

    Replace SDK

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 一个基于[CoolQ平台](https://cqp.cc/forum.php)的Douyu直播间开播提醒工具。
 
-本插件基于C#语言编写(使用了来自论坛ID:pianzide1117的[框架](https://cqp.cc/t/33399))
+本插件基于C#语言编写(感谢来自论坛ID:947295340的[框架](https://github.com/Jie2GG/Native))
 
-目前代码整体表现仍不完美，欢迎各位提出修改意见或直接上手(有dalao可以帮忙直接改成VC++我更开心2333)
+目前代码整体表现仍不完美，欢迎各位提出修改意见或直接PR一波233。
 
 使用组件：
 
 SQLitePCL [license](https://archive.codeplex.com/?p=sqlitepcl)
-
-AutoFac [MIT license](https://opensource.org/licenses/mit-license.php)
 
 Newtonsoft.Json [MIT license](https://raw.githubusercontent.com/JamesNK/Newtonsoft.Json/master/LICENSE.md)
---
+++ cn.mikualpha.douyu/Native.Demo/App.Event/Event_PrivateMessage.cs
@@ -0,0 +1,112 @@
+﻿using Native.Sdk.Cqp.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Native.Demo.App.Model;
+
+namespace Native.Demo.App.Event
+{
+	public class Event_PrivateMessage
+	{
+		#region --字段--
+		private static Event_PrivateMessage _instance = new Event_PrivateMessage();
+		#endregion
+
+		#region --属性--
+		/// <summary>
+		/// 获取 Event_ReceiveMessage 实例对象
+		/// </summary>
+		public static Event_PrivateMessage Instance { get => _instance; }
+		#endregion
+
+		#region --构造函数--
+		/// <summary>
+		/// 隐藏构造函数
+		/// </summary>
+		private Event_PrivateMessage()
+		{
+
+		}
+		#endregion
+
+		#region --公开方法--
+		/// <summary>
+		/// Type=21 好友消息
+		/// </summary>
+		/// <param name="sender">触发此事件的对象</param>
+		/// <param name="e">附加的参数</param>
+		public void ReceiveFriendMessage(object sender, PrivateMessageEventArgs e)
+		{
+            //本子程序会在酷Q【线程】中被调用，请注意使用对象等需要初始化(CoInitialize,CoUninitialize)。
+            //这里处理消息
+
+            processMsg(e);
+        }
+
+		/// <summary>
+		/// Type=21 在线状态消息
+		/// </summary>
+		/// <param name="sender">触发此事件的对象</param>
+		/// <param name="e">附加的参数</param>
+		public void ReceiveOnlineStatusMessage(object sender, PrivateMessageEventArgs e)
+		{
+			//本子程序会在酷Q【线程】中被调用，请注意使用对象等需要初始化(CoInitialize,CoUninitialize)。
+			//这里处理消息
+
+			e.Handled = false;  //关于返回说明, 请参见 "Event_ReceiveMessage.ReceiveFriendMessage" 方法
+		}
+
+		/// <summary>
+		/// Type=21 群私聊消息
+		/// </summary>
+		/// <param name="sender">触发此事件的对象</param>
+		/// <param name="e">附加的参数</param>
+		public void ReceiveGroupPrivateMessage(object sender, PrivateMessageEventArgs e)
+		{
+            //本子程序会在酷Q【线程】中被调用，请注意使用对象等需要初始化(CoInitialize,CoUninitialize)。
+            //这里处理消息
+            processMsg(e);
+		}
+
+		/// <summary>
+		/// Type=21 讨论组私聊消息
+		/// </summary>
+		/// <param name="sender">触发此事件的对象</param>
+		/// <param name="e">附加的参数</param>
+		public void ReceiveDiscussPrivateMessage(object sender, PrivateMessageEventArgs e)
+		{
+            //本子程序会在酷Q【线程】中被调用，请注意使用对象等需要初始化(CoInitialize,CoUninitialize)。
+            //这里处理消息
+            processMsg(e);
+        }
+        #endregion
+
+        #region --自用函数--
+        private void processMsg(PrivateMessageEventArgs context)
+        {
+            if (context.Msg.Contains("/斗鱼订阅-"))
+            {
+                int index = context.Msg.IndexOf('-');
+                string room = context.Msg.Substring(index + 1);
+                DouyuCheck.getInstance().SubscribeByUser(context.FromQQ.ToString(), room);
+                EnApi.Instance.SendPrivateMessage(context.FromQQ, "订阅主播[" + DouyuCheck.getInstance().getOwner(room) + "]成功!");
+                context.Handled = true;
+                return;
+            }
+
+            if (context.Msg.Contains("/斗鱼取消订阅-"))
+            {
+                int index = context.Msg.IndexOf('-');
+                string room = context.Msg.Substring(index + 1);
+                DouyuCheck.getInstance().Desubscribe(context.FromQQ.ToString(), room);
+                EnApi.Instance.SendPrivateMessage(context.FromQQ, "取消订阅主播[" + DouyuCheck.getInstance().getOwner(room) + "]成功!");
+                context.Handled = true;
+                return;
+            }
+
+            context.Handled = false;
+        }
+        #endregion
+    }
+}
